cbd2d1cb4e57814ecd7fdfee5a51e149861a5b78
nithu8341 <81219554+nithu8341@users.noreply.github.com>
2025-04-07 12:04:42 +0530

    Delete mega projet directory

--- mega projet/musiclibrary.py
+++
@@ -1,6 +0,0 @@
-music = {
-    "whoareyou": "https://youtu.be/qINq-ReJeMg",
-    "follow": "https://youtu.be/MFX1wENYymA",
-    "blindinglights": "https://youtu.be/4NRXx6U8ABQ",
-    "starboy": "https://youtu.be/34Na4j8AVgA"
-}
